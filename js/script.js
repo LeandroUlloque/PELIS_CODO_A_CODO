85e7334aa60467ea4c6a0c17ece71f4a6bfbc3f4
LeandroUlloque <63380623+LeandroUlloque@users.noreply.github.com>
2023-12-08 11:49:50 -0300

    corregi varios errores de cierres de etiquetas 2

--- js/script.js
+++ js/script.js
@@ -1,67 +1,66 @@
 // SCRIPT DEL HEADER
-const menu = document.querySelector('i');
-const links = document.querySelector('nav');
+const menu = document.querySelector("i");
+const links = document.querySelector("nav");
 
-menu.addEventListener("click", () =>{
-    links.classList.toggle("mobile-menu")
+menu.addEventListener("click", () => {
+  links.classList.toggle("mobile-menu");
 });
 // FIN DEL HEADER
 
 // SCRIPT SLIDER
-if(document.querySelector('#container-slider')){
-    setInterval('fntExecuteSlide("next")',5000);
- }
- //------------------------------ LIST SLIDER -------------------------
- if(document.querySelector('.listslider')){
-    let link = document.querySelectorAll(".listslider li a");
-    link.forEach(function(link) {
-       link.addEventListener('click', function(e){
-          e.preventDefault();
-          let item = this.getAttribute('itlist');
-          let arrItem = item.split("_");
-          fntExecuteSlide(arrItem[1]);
-          return false;
-       });
-     });
- }
- 
- function fntExecuteSlide(side){
-     let parentTarget = document.getElementById('slider');
-     let elements = parentTarget.getElementsByTagName('li');
-     let curElement, nextElement;
- 
-     for(var i=0; i<elements.length;i++){
- 
-         if(elements[i].style.opacity==1){
-             curElement = i;
-             break;
-         }
-     }
-     if(side == 'prev' || side == 'next'){
- 
-         if(side=="prev"){
-             nextElement = (curElement == 0)?elements.length -1:curElement -1;
-         }else{
-             nextElement = (curElement == elements.length -1)?0:curElement +1;
-         }
-     }else{
-         nextElement = side;
-         side = (curElement > nextElement)?'prev':'next';
- 
-     }
-     //RESALTA LOS PUNTOS
-     let elementSel = document.getElementsByClassName("listslider")[0].getElementsByTagName("a");
-     elementSel[curElement].classList.remove("item-select-slid");
-     elementSel[nextElement].classList.add("item-select-slid");
-     elements[curElement].style.opacity=0;
-     elements[curElement].style.zIndex =0;
-     elements[nextElement].style.opacity=1;
-     elements[nextElement].style.zIndex =1;
- }
+if (document.querySelector("#container-slider")) {
+  setInterval('fntExecuteSlide("next")', 5000);
+}
+//------------------------------ LIST SLIDER -------------------------
+if (document.querySelector(".listslider")) {
+  let link = document.querySelectorAll(".listslider li a");
+  link.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      let item = this.getAttribute("itlist");
+      let arrItem = item.split("_");
+      fntExecuteSlide(arrItem[1]);
+      return false;
+    });
+  });
+}
+
+function fntExecuteSlide(side) {
+  let parentTarget = document.getElementById("slider");
+  let elements = parentTarget.getElementsByTagName("li");
+  let curElement, nextElement;
+
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].style.opacity == 1) {
+      curElement = i;
+      break;
+    }
+  }
+  if (side == "prev" || side == "next") {
+    if (side == "prev") {
+      nextElement = curElement == 0 ? elements.length - 1 : curElement - 1;
+    } else {
+      nextElement = curElement == elements.length - 1 ? 0 : curElement + 1;
+    }
+  } else {
+    nextElement = side;
+    side = curElement > nextElement ? "prev" : "next";
+  }
+  //RESALTA LOS PUNTOS
+  let elementSel = document
+    .getElementsByClassName("listslider")[0]
+    .getElementsByTagName("a");
+  elementSel[curElement].classList.remove("item-select-slid");
+  elementSel[nextElement].classList.add("item-select-slid");
+  elements[curElement].style.opacity = 0;
+  elements[curElement].style.zIndex = 0;
+  elements[nextElement].style.opacity = 1;
+  elements[nextElement].style.zIndex = 1;
+}
 //  FIN SCRIPT SLIDER
 
 // FUNCION MENSAJE
 let btnReproducir = document.getElementById("btnReproducir");
 btnReproducir.addEventListener("click", (event) => {
-    alert("JAJA PAGÁ NETFLIX");
+  alert("JAJA PAGÁ NETFLIX");
 });
