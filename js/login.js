ba80d57d4fffc9c29d32a7a3019a0458287b4d33
unknown <econsorte@hotmail.com>
2023-12-10 01:13:51 -0300

    esooo

---
+++ js/login.js
@@ -0,0 +1,10 @@
+function go(){
+
+if (document.form.password.value=='admin' && document.form.login.value=='admin'){ 
+        document.form.submit(); 
+    } 
+    else{ 
+         alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+    } 
+} 
+ 
---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.getElementById("header").innerHTML=`  <div class="nav1" id="header">
+<a class="logo" href="./index.html"><img src="./img/logo.png" alt="logo" height="68px"></a>
+<nav>
+    <a href="./index.html">Inicio</a>
+    <a href="./pages/contacto.html">Contacto</a>
+    
+    
+</nav>
+<i class="bi bi-list"></i>
+</div>
+<div class="nav2">
+<div>
+    <a class="fondo" href="./login.html">Login</a>
+    <a class="fondo2" href="#">Registrarte</a>
+</div>
+<div>
+`
