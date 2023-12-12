3ac52e04ef41a2eec30aed53682ef0ddea68907f
econsorte <econsorte@hotmail.com>
2023-12-11 21:16:46 -0300

    anadimosfffff

--- js/login.js
+++ js/login.js
@@ -1,10 +1,21 @@
 function go(){
 
-if (document.form.password.value=='admin' && document.form.login.value=='admin'){ 
-        document.form.submit(); 
+    if (document.form.password.value=='admin' && document.form.login.value=='admin'){ 
+        sessionStorage.setItem("pelis", "si" );
+        location.href= "pelis.html";
+            
+        } 
+        else  if(document.form.password.value=='user' && document.form.login.value=='user'){ 
+
+            sessionStorage.setItem("user", "si" );
+            location.href= "pelisuser.html";
+           
+        }else{
+            alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+
+        } 
+
     } 
-    else{ 
-         alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
-    } 
-} 
- 
+    
+
+     
---
+++ js/sesion.js
@@ -0,0 +1,3 @@
+if(!sessionStorage.getItem("pelis")){
+    location.href = "./login.html"; 
+}
